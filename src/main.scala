555f97530006262e144cdbe5d5c2c165722590c2
GuillaumeKoenigTncy <guillaume.koenig@telecomnancy.net>
2015-11-16 09:01:18 +0100

    Merge to master problem removed

--- src/main.scala
+++ src/main.scala
@@ -1,7 +1,6 @@
 /**
  * @author Utilisateur
  */
-<<<<<<< HEAD
 object main extends App {
 
   implicit class IntToBase(val digits: String) extends AnyVal {
@@ -62,36 +61,3 @@ object main extends App {
   wrappedImage.saveImage(outputFile);
 
 }
-=======
-object main extends App{
-  
-    // Import the API library
-    // v 11.0
-   import com.tncy.top.image.ImageWrapper;
-
-   // Source image file
-   var fileName : String = "imgs/ImagesTests/2.jpg";
-
-   // Load wrapped image
-   var wrappedImage : ImageWrapper = new ImageWrapper(fileName);
-   // Get the image
-   var image2D : Array[Array[Int]] = wrappedImage.getImage();
-
-   // Print image height and width
-   println("The image height is: " + wrappedImage.height + " px.");
-   println("The image width is: " + wrappedImage.width + " px.");
-
-   // Modify it
-   for (row <- 0 to 40) {
-     for (col <-0 to 80) {
-       image2D(row)(col) = 0x000000FF; // Set these pixels to RGB blue
-     }
-   }
-
-   // Destination image file - Note that the image file name must contain the file type extension for the image to be saved correctly.
-   var outputFile : String = "outputImage.jpg";
-   // Save the result
-   wrappedImage.saveImage(outputFile);
-  
-}
->>>>>>> origin/master
