0dd78cb74d1d9758f5a732e36b7c6e75f886296f
GuillaumeKoenigTncy <guillaume.koenig@telecomnancy.net>
2016-01-02 22:03:43 +0100

    Remove Noise

--- src/main.scala
+++ src/main.scala
@@ -7,160 +7,111 @@ import java.awt.image.BufferedImage
 
 object main extends App {
 
-  // Import the API library
-  // v 11
   import com.tncy.top.image.ImageWrapper;
 
-  // Fonction qui complète un input par un char sur les bits à gauche dans la limite de len
-<<<<<<< HEAD
-  def completeBy(input: String, char: Char, len: Int): String = {
-=======
- /*  def completeBy(input: String, char: Char, len: Int): String = {
->>>>>>> refs/remotes/origin/KLEIN
-    var input_tmp = input;
-    for (n <- 0 to len - input.length() - 1) {
-      input_tmp = char + input_tmp;
-    }
-    return input_tmp;
-  }
-
-  // Fonction qui convertit une chaine input binaire en un Int par la méthode des poids pondérés
-  def binaryToInt(input: String): Int = {
-    var number = 0;
-    var counter = 0;
-    var weight = input.length() - 1;
-
-    while (counter <= input.length() - 1) {
-      if (input(counter).compare('1') == 0) {
-        number = number + math.pow(2, weight).toInt
-      }
-      counter = counter + 1;
-      weight = weight - 1;
-    }
-    return number;
-  }
-
-  // Fonction qui passe l'image en niveau de gris
-  // Nécessite que l'image soit définit avant l'appel de la fonction !
-  def RGBToGrey(){
-    for (row <- 0 to wrappedImage.height - 1) {
-      for (col <- 0 to wrappedImage.width - 1) {
-
-        // (MSB) [TTTT TTTT] RRRR RRRR GGGG GGGG BBBB BBBB (LSB)
-        var color = Integer.toString(image2D(row)(col), 2);
-        var len = color.length();
-        var lol = 32 - len
-
-        if (color(0).compare('-') == 0) {
-          color = color.replace('-', '1');
-          for (n <- 0 to lol - 1) {
-            color = completeBy(color, '1', 32);
-          }
-          color = color.replace('1', '2');
-          color = color.replace('0', '1');
-          color = color.replace('2', '0');
-<<<<<<< HEAD
-
-=======
-          
->>>>>>> refs/remotes/origin/KLEIN
-          if (color(31).compare('0') == 0) {
-            color = color.substring(0, 31) + '1';
-          } else {
-            color = color.substring(0, 31) + '0';
-          }
-        } else {
-          for (n <- 0 to lol - 1) {
-            color = completeBy(color, '0', 32);
-          }
-        }
-
-        var DR = Integer.valueOf(color.substring(8, 16), 2);
-        var DG = Integer.valueOf(color.substring(16, 24), 2);
-        var DB = Integer.valueOf(color.substring(24, 32), 2);
-        var nuanceDeGris = 0.299 * DR + 0.587 * DG + 0.114 * DB;
-        var nuanceDeGrisS = completeBy(nuanceDeGris.toInt.toBinaryString, '0', 8)
-        var finalNuanceDeGris = "11111111" + nuanceDeGrisS + nuanceDeGrisS + nuanceDeGrisS
-
-        image2D(row)(col) = binaryToInt(finalNuanceDeGris);
+  def codeGrey() {
+    for (i <- 0 to image2D.length - 1) {
+      for (j <- 0 to image2D(1).length - 1) {
+        var T = (image2D(i)(j) & 0xFF000000) // to les bit T à 1
+        var R = (image2D(i)(j) & 0x00FF0000) >> 16 // to les bit R à 1
+        var G = (image2D(i)(j) & 0x0000FF00) >> 8 // to les bit G à 1
+        var B = (image2D(i)(j) & 0x000000FF) // to les bit B à 1
+        var RGB = (0.299 * R + 0.587 * G + 0.114 * B).toInt; // FF + FF + FF = 7f     
+        image2D(i)(j) = T + (RGB * 65536 + RGB * 256 + RGB); // 192  000 000 -> 127 127 127
       }
     }
   }
+  def printImg(name: String) {
+    var outputFile: String = name;
+    wrappedImage.saveImage(outputFile);
+  }
+  def sobleBy3() {
+    var sobel_x: Array[Array[Double]] = Array(
+      Array(-1, 0, 1),
+      Array(-2, 0, 2),
+      Array(-1, 0, 1));
 
-  var fileName: String = "imgs/ImagesReelles/1_GoogleMaps.png"; // Source de l'image
+    var sobel_y: Array[Array[Double]] = Array(
+      Array(1, 2, 1),
+      Array(0, 0, 0),
+      Array(-1, -2, -1));
+
+    for (x <- 1 to wrappedImage.height - 2) {
+      for (y <- 1 to wrappedImage.width - 2) {
+
+        var a = (image2D(x - 1)(y - 1) & 0x000000FF)
+        var b = (image2D(x)(y - 1) & 0x000000FF)
+        var c = (image2D(x + 1)(y - 1) & 0x000000FF)
+        var d = (image2D(x - 1)(y) & 0x000000FF)
+        var e = (image2D(x)(y) & 0x000000FF)
+        var f = (image2D(x + 1)(y) & 0x000000FF)
+        var g = (image2D(x - 1)(y + 1) & 0x000000FF)
+        var h = (image2D(x)(y + 1) & 0x000000FF)
+        var i = (image2D(x + 1)(y + 1) & 0x000000FF)
+
+        // println(a + " | " + b + " | " + c)
+
+        var pixel_x =
+          (sobel_x(0)(0) * a) + (sobel_x(0)(1) * b) + (sobel_x(0)(2) * c) +
+            (sobel_x(1)(0) * d) + (sobel_x(1)(1) * e) + (sobel_x(1)(2) * f) +
+            (sobel_x(2)(0) * g) + (sobel_x(2)(1) * h) + (sobel_x(2)(2) * i);
+        var pixel_y =
+          (sobel_y(0)(0) * a) + (sobel_x(0)(1) * b) + (sobel_x(0)(2) * c) +
+            (sobel_y(1)(0) * d) + (sobel_x(1)(1) * e) + (sobel_x(1)(2) * f) +
+            (sobel_y(2)(0) * g) + (sobel_x(2)(1) * h) + (sobel_x(2)(2) * i);
+
+        var lol = (Math.sqrt((pixel_x * pixel_x) + (pixel_y * pixel_y)).ceil).toInt
+
+        image2D2(x)(y) = 0xFF000000 + (lol * 65536 + lol * 256 + lol).toInt;
+
+        //println((image2D(x)(y) & 0xFF000000)>>24);
+      }
+    }
+  }
+  def noiseRemove() {
+    for (x <- 1 to wrappedImage.height - 2) {
+      for (y <- 1 to wrappedImage.width - 2) {
+
+        var a = (image2D(x - 1)(y - 1) & 0x000000FF)
+        var b = (image2D(x)(y - 1) & 0x000000FF)
+        var c = (image2D(x + 1)(y - 1) & 0x000000FF)
+        var d = (image2D(x - 1)(y) & 0x000000FF)
+        var e = (image2D(x)(y) & 0x000000FF)
+        var f = (image2D(x + 1)(y) & 0x000000FF)
+        var g = (image2D(x - 1)(y + 1) & 0x000000FF)
+        var h = (image2D(x)(y + 1) & 0x000000FF)
+        var i = (image2D(x + 1)(y + 1) & 0x000000FF)
+
+        var moy = (a+b+d+c+e+f+g+h+i)/9;
+
+        image2D(x)(y) = moy;
+      }
+    }
+  }
+  
+  // Note ~A = complement à 2 de A, 00 00 00 11 & color = code B
+
+  var fileName: String = "imgs/ImagesReelles/2_GoogleMaps.png"; // Source de l'image
+  //var fileName: String = "imgs/ImagesTests/8.jpg"; // Source de l'image
+  //var fileName: String = "imgs/Valve_original.png"; 
   var wrappedImage: ImageWrapper = new ImageWrapper(fileName); // Chargement
   var image2D: Array[Array[Int]] = wrappedImage.getImage(); // Mise en matrice
+
+  var wrappedImage2: ImageWrapper = new ImageWrapper(fileName); // Chargement
+  var image2D2: Array[Array[Int]] = wrappedImage2.getImage(); // Mise en matrice
 
   println("Informations : " + wrappedImage.height + " px par " + wrappedImage.width + " px.");
+
+  noiseRemove()
   
-  RGBToGrey();
-  
-  // Destination image file - Note that the image file name must contain the file type extension for the image to be saved correctly.
+  sobleBy3()
+
   var outputFile: String = "outputImage.jpg";
-  // Save the result
   wrappedImage.saveImage(outputFile);
 
-<<<<<<< HEAD
-=======
+  var outputFile2: String = "outputImage2.jpg";
+  wrappedImage2.saveImage(outputFile2);
 
-   //On s'attaque aux contrastes  
-  //Soit N nb de pixel
-  var n = wrappedImage.height * wrappedImage.width
-  var image2DCont: Array[Array[Int]] = wrappedImage.getImage(); 
-  
-  
-def minMaxGrey(){
-  var rgb = 0;
-    for(i <- 0 to wrappedImage.width -1) 
-    {
-      for(j<- 0 to wrappedImage.height -1)
-      {
-        rgb = image2D(i)(j);
-      }
-    }
-    
-    
-  }
-  
- def greyToContrast(){
-    for(i <- 0 to image2D.length -1) // largeur d'image2D en px
-    {
-      for(j<- 0 to image2D(1).length -1) //longueur d'image2D en px
-      {
-        image2DCont(i)(j) = (255 * (image2D(i)(j) - min) / (max - min));
-      }
-    }
-    
-    
-  }*/
-// Note ~A = complement à 2 de A, 00 00 00 11 & color = code B
+  //printImg("outputImage.jpg");
 
-
-  var fileName: String = "imgs/ImagesReelles/1_GoogleMaps.png"; // Source de l'image
-  var wrappedImage: ImageWrapper = new ImageWrapper(fileName); // Chargement
-  var image2D: Array[Array[Int]] = wrappedImage.getImage(); // Mise en matrice
-  
-def codeGrey(){
-  for(i<- 0 to image2D.length-1  ) {
-    for(j <- 0 to image2D(1).length-1 ) {
-      var T = (image2D(i)(j) & 0xFF000000) // to les bit T à 1
-      var R = (image2D(i)(j) & 0x00FF0000)>>16 // to les bit R à 1
-      var G = (image2D(i)(j) &  0x0000FF00)>>8// to les bit G à 1
-      var B = image2D(i)(j) & 0x000000FF // to les bit B à 1
-      var RGB = (0.299 * R + 0.587 * G + 0.114 * B).toInt; // FF + FF + FF = 7f     
-      image2D(i)(j) = T + (RGB*256*256 + RGB*256 + RGB); // 192  000 000 -> 127 127 127
-    }
-  }
-}
-  def printImg(name:String){
-    
-   // Destination image file - Note that the image file name must contain the file type extension for the image to be saved correctly.
-  var outputFile: String = name;
-  // Save the result
-  wrappedImage.saveImage(outputFile); 
-  }
-  //Debut du code
-  codeGrey();
-  printImg("outputImage.jpg");
->>>>>>> refs/remotes/origin/KLEIN
 }
